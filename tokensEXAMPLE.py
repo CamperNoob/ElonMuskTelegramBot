c2899f0d99b90b889042f2044c8017e77009a87d
Mykhailo Yudin <misha.yudin19@gmail.com>
2021-08-13 08:34:30 +0300

    Add tokenfile example
    Currently in py format

---
+++ tokensEXAMPLE.py
@@ -0,0 +1,7 @@
+TelegramTOKEN = ""
+
+# customer key, customer secret, access token, access token secret
+TwitterTOKEN = {'ckey': '',
+                'csecret': '',
+                'at': '',
+                'atsecret': ''}
